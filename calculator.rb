c96c7b3d68e2b705490fe2217856cc2ad9500f4f
foolproof-htb <fujisawa.htb@gmail.com>
2023-10-15 10:50:54 +0900

    :sparkles: 式を計算するクラスとを追加
    動作確認のため、計算クラスを使用するファイルも合わせて追加

---
+++ calculator.rb
@@ -0,0 +1,11 @@
+require './lexer'
+require './parser'
+
+# 式を受け取って計算する
+class Calculator
+  def evaluate(expression)
+    lexer = Lexer.new(expression)
+    parser = Parser.new(lexer)
+    parser.parse
+  end
+end
---
+++ run.rb
@@ -0,0 +1,5 @@
+require './calculator'
+
+calc = Calculator.new
+result = calc.evaluate('1 + 2 * 3')
+puts "result: #{result}"
